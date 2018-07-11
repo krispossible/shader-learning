7d53c92ef38d267600c82f827fa3a263d5d27089
Kristof Somlyai <Kristof.Somlyai@POSSIBLE.com>
2018-07-11 14:22:41 +0200

    - project created

---
+++ Assets/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	[SerializeField] private float _speed;
+
+	void Update()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		transform.position = Vector3.MoveTowards(transform.position, transform.position + new Vector3(horizontal, 0, vertical), Time.deltaTime * _speed);
+	}
+}
